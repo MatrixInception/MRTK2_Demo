9f6be5b644bfd266ded4439d179bc0d7b814c05c
skyzdev <sky@matrixinception.com>
2019-08-31 19:30:40 -0400

    Commit 1.2 - testing with AR Foundation

---
+++ Assets/TeleportManagerMI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Teleport;
+using Microsoft.MixedReality.Toolkit.Input;
+
+public class TeleportManagerMI : MonoBehaviour
+{
+    TeleportPointer tPointer;
+    TeleportHotSpot tHotspot;
+    public TeleportCursor tCursor; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnableTeleportSystem();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DisableTeleportSystem()
+    {
+        CoreServices.TeleportSystem.Disable();
+    }
+
+    void EnableTeleportSystem()
+    {
+        CoreServices.TeleportSystem.Enable();
+    }
+}
